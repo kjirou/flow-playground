a94bd0876b2723bd521e7733d8f574dba8227408
kjirou <kjirou.web@gmail.com>
2017-02-18 00:36:23 +0900

    Check normal annotation

--- es/foo.js
+++ es/foo.js
@@ -3,5 +3,17 @@
 import { add, subtract } from './bar';
 
 const added = add(1 ,2);
-
 const subtracted = subtract(2 ,1);
+
+export const x = added + subtracted;
+
+export const calculate = (type: string, a: number, b: number) => {
+  switch (type) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+  }
+
+  throw new Error('Invalid type');
+};
