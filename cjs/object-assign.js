85ef9b27d5daf9cf61272bbccd2ea031fe62d70e
kjirou <kjirou.web@gmail.com>
2017-02-25 17:07:25 +0900

    Add samples of "Sealed object types"

--- cjs/object-assign.js
+++ cjs/object-assign.js
@@ -8,6 +8,28 @@ Object.assign({}, { a: null }, { a: 1 });
 
 // ----
 
+//
+// Sealed | Unsealed Object Types
+//
+// https://flowtype.org/docs/objects.html#sealed-object-types
+//
+
+// Good
+const obj = {};
+obj.x;
+obj.x = 1;
+// Bad
+//   It is considered to be a sealed object
+//const obj: {} = {};
+//obj.x;
+//obj.x = 1;
+// Bad
+//   Non-empty object is also considered as such
+//const obj = { y: 2 };
+//obj.x = 1;
+
+// ----
+
 // Good
 (Object.assign({}, { a: 1 }, { b: 2 }): { a: 1, b: 2 });
 // Bad
