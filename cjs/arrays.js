bca7333e3eda88eef97fefc6f921fd9f0a4f9b2a
kjirou <kjirou.web@gmail.com>
2017-02-28 22:40:37 +0900

    Add a sample

---
+++ cjs/arrays.js
@@ -0,0 +1,13 @@
+// @flow
+
+//
+// Manual: https://flowtype.org/docs/arrays.html
+//
+
+
+//
+// Shorthand syntax
+//
+const oneNest/*:number[]*/ = [1, 2];
+const twoNests/*:number[][]*/ = [[1, 1], [2, 2]];
+const threeNests/*:number[][][]*/ = [[[1, 1], [2, 2]], [[3, 3], [3, 3]]];
