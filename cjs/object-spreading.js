ba8899efb8eb728f981911ecb05fc983cc1b2131
kjirou <sorenariblog@gmail.com>
2017-03-23 00:13:32 +0900

    Add a sample of the Object spreading

---
+++ cjs/object-spreading.js
@@ -0,0 +1,55 @@
+// @flow
+
+//
+// プロパティを足す場合は
+//
+// type X = {
+//   a: number,
+// };
+//
+// type Y = {
+//   ...X,
+//   b: number,
+// };
+//
+// ...X の下に足したいプロパティを付与しないと足せない
+//
+
+/*::
+type Unit = {
+  hp: number,
+};
+
+type AllyUnit = {
+  ...Unit,
+  type: 'ALLY',
+  mp: number,
+};
+ */
+
+// Good
+const ally/*:AllyUnit*/ = {
+  type: 'ALLY',
+  hp: 1,
+  mp: 2,
+};
+
+// Bad
+//const ally2/*:AllyUnit*/ = {
+//  type: 'ALLY',
+//  hp: 'a',
+//  mp: 2,
+//};
+
+// Bad
+//const ally3/*:AllyUnit*/ = {
+//  type: 'ALLY',
+//  hp: 1,
+//  mp: 2,
+//};
+
+// Good! なんで!?
+const ally4/*:AllyUnit*/ = {
+  type: 'ALLY',
+  mp: 2,
+};
