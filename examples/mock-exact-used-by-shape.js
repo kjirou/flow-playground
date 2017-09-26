443e534969c437fe4a87788ebbb7257d7aa1dc7b
kjirou <sorenariblog@gmail.com>
2017-09-26 13:15:24 +0900

    Update a sample

--- examples/mock-exact-used-by-shape.js
+++ examples/mock-exact-used-by-shape.js
@@ -1,9 +1,19 @@
 // @flow
 
 //
+// $Exact だとこの宣言がエラーになるので、別の手段を探したい
+// ```
+// const obj: {| foo: 0 |} = Object.assign({}, {foo: 0})
+// ```
+//
 // $Shape と $Exact の違いの参考資料
 // https://qiita.com/kinzal/items/e1898c89af5618e18334
 //
+
+
+// 怒られる
+// NG: "object literal. Inexact type is incompatible with exact type ..."
+//const obj: {| foo: 0 |} = Object.assign({}, {foo: 0});
 
 type MyExact<T> = T & $Shape<T>;
 
