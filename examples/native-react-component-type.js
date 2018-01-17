833216f227b2db0bf73170133d3e7d152bfecafa
kjirou <sorenariblog@gmail.com>
2018-01-17 16:08:38 +0900

    Add a sample

---
+++ examples/native-react-component-type.js
@@ -0,0 +1,21 @@
+// @flow
+
+import React from 'react';
+
+
+type P = {a: number};
+
+
+// TODO: なんで動かないんだろうこれは
+//function Hoge(p: P) {
+//  return React.createElement('div', p);
+//}
+//(Hoge: React$Component<P>);
+//
+//
+//class Fuga extends React.Component<P> {
+//  render() {
+//    return React.createElement('div', this.props);
+//  }
+//}
+//(Fuga: React$Component<P>);
